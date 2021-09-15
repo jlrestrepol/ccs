23091c36591d5c0f19f77f7aa97336d523c0ee51
Restrepo Lopez <rlopez@hpcl8001.biochem.mpg.de>
2021-09-15 17:46:50 +0200

    changes pdb filename

--- code/distance_matrix.py
+++ code/distance_matrix.py
@@ -3,7 +3,6 @@ import numpy as np
 import os
 
 pdb_id = "ranked_0"
-pdb_filename = "./predictions/reduced_dbs/test7/ranked_0.pdb"
+pdb_filename = "/fs/pool/pool-cox-projects-fold/predictions/reduced_dbs/test_roseta/ranked_0.pdb"
 
-#print(os.getcwd())
 structure = Bio.PDB.PDBParser().get_structure(pdb_id, pdb_filename)
