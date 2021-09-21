b05f942e0d7f54c982dc55a501cc338841b5aa54
jlrestrepol <jlrestrepol@gmail.com>
2021-09-21 18:14:27 +0200

    Preliminary buggy version of sequence to distance matrix matching

--- code/peptide_shape.py
+++ code/peptide_shape.py
@@ -8,10 +8,11 @@ import numpy as np
 import pandas as pd
 import Bio.PDB
 from distance_matrix import calc_dist_matrix_numpy
-
+import os
 
 #Set paths for reading MaxQuant output
-folder_path = '../Data/MaxQuant/CAEEL/'
+organism = 'ECOLI'
+folder_path = '../Data/MaxQuant/'+organism+'/'
 file_path = 'peptides.txt'
 
 df_peptides = pd.read_csv(folder_path+file_path, sep='\t')#Read data in
@@ -19,27 +20,44 @@ df_peptides = pd.read_csv(folder_path+file_path, sep='\t')#Read data in
 df_peptides_filtered = df_peptides[df_peptides['Reverse']!='+']#Filter out reversed peptides
 
 df_pep_prot = df_peptides_filtered.loc[:,['Sequence','Proteins', 'Mass', 'Charges']]#Keep only useful features
-# %%
+
+df_pep_prot['Proteins'] = df_pep_prot['Proteins'].apply(lambda x: x.split(';')[0])#Keep only the first protein - might have to change it
+seq_list = df_pep_prot['Sequence']
 #Set paths for reading Alpha Fold 2 output from
-folder_path = '../Data/AlphaFold/CAEEL/AF-'
+folder_path = '../Data/AlphaFold/'+organism+'/AF-'
 suffix = '-F1-model_v1.pdb'
+submatrix_list = []
 
-#Get the data from the .pdb file
-parser = Bio.PDB.PDBParser()# create parser
-structure_alpha = parser.get_structure("alpha",folder_path+df_pep_prot['Proteins'][0]+suffix)#get structure
-model_alpha = structure_alpha[0]#get model
+#Iterate over the protein names
+for index, (label, prot_id) in enumerate(df_pep_prot['Proteins'].items()):
+    #Get the data from the .pdb file
+    
+    file_path = folder_path+prot_id+suffix
+    if not os.path.exists(file_path):
+        print(prot_id+' not found, skeaping to next protein')
+        submatrix_list.append(np.nan)
+        continue
+    print(prot_id)
+    parser = Bio.PDB.PDBParser()# create parser
+    structure_alpha = parser.get_structure("alpha",folder_path+prot_id+suffix)#get structure
+    model_alpha = structure_alpha[0]#get model
 
-#Calculate distance matrix
-dist_matrix_alpha = calc_dist_matrix_numpy(model_alpha["A"], model_alpha["A"])
-#%%
-#Get sequences from .pdb file(proteins) and from MaxQuant's output(peptide)
-ppb = Bio.PDB.PPBuilder().build_peptides(structure_alpha)[0]
-seq_prot = ppb.get_sequence()
-seq_pep = df_pep_prot['Sequence'][0]
-#Find position of peptide in protein
-start_index = seq_prot.find(seq_pep)
-final_index = start_index + len(seq_pep)
-# %%
-#Extract submatrix
-submatrix = dist_matrix_alpha[start_index: final_index, start_index:final_index]
+    #Calculate distance matrix
+    dist_matrix_alpha = calc_dist_matrix_numpy(model_alpha["A"], model_alpha["A"])
+
+    #Get sequences from .pdb file(proteins) and from MaxQuant's output(peptide)
+    ppb = Bio.PDB.PPBuilder().build_peptides(structure_alpha)[0]
+    seq_prot = ppb.get_sequence()
+    seq_pep = seq_list[index]
+    #Find position of peptide in protein
+    start_index = seq_prot.find(seq_pep)
+    final_index = start_index + len(seq_pep)
+
+    #Extract submatrix
+    submatrix = dist_matrix_alpha[start_index: final_index, start_index:final_index]
+    submatrix_list.append(submatrix)
+
+df_pep_prot['Distance_matrix'] = submatrix_list
+results_folder = "../Data/Combined/"
+df_pep_prot.to_pickle(results_folder+organism+'.pkl')
 # %%
