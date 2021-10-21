d23e118a023d44de139d4f29a85cafa0e2153ea4
Juan Restrepo Lopez <rlopez@biochem.mpg.de>
2021-10-21 14:15:12 +0200

    Adds highlight in 2d scatter plot

--- code/linear_fit.py
+++ code/linear_fit.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import Bio.PDB
+from Bio.PDB.Polypeptide import PPBuilder
 from itertools import chain
 from skspatial.objects import Line, Points
 import matplotlib.pyplot as plt
@@ -41,6 +42,7 @@ def one_fit(file_name = '/200/ranked_1.pdb'):
 def main():
     data_folder = '/fs/pool/pool-cox-projects-fold/predictions/full_dbs/fasta'
     rmse_list = []
+    seq_list = []
     for root,dirs,files in os.walk(data_folder):
         #go through roots that don't end in fasta or msas and only take ones that are completed
         if not (root.endswith(("fasta","msas"))) and len(files)==23:
@@ -55,6 +57,9 @@ def main():
 
                 if best_score > 70: #Only take files whose score is above 70
                     structure_alpha = Bio.PDB.PDBParser().get_structure("alpha",root+"/ranked_1.pdb")
+                    ppb=PPBuilder()
+                    seq = str([pp.get_sequence() for pp in ppb.build_peptides(structure_alpha)][0])
+                    seq_list.append(seq)
                     model_alpha = structure_alpha[0]
                     chain_a = model_alpha['A']
                     coords_one = np.fromiter( chain.from_iterable(res["CA"].coord for res in chain_a), dtype = 'f8', count = -1).reshape((-1, 3))
@@ -64,14 +69,38 @@ def main():
                     rmse = np.sqrt((residuals*residuals).sum()/coords_one.shape[0])
                     rmse_list.append(rmse)
     
+    fig = plt.figure(figsize = (12,18))
+    ax=fig.add_subplot(111)
     ax = sns.histplot(rmse_list)
     ax.set_xlabel('RMSE')
     ax.set_ylabel('Counts')
     ax.set_title('Distribution of RMSE-PLDDT>70')
     
-    return rmse_list
+    return rmse_list, seq_list
 #%%
 if __name__ == '__main__':
-    main()
+    rmse_list, seq_list = main()
+    
+    rmse_array = np.array(rmse_list)
+    seq_array = np.array(seq_list)
+    second_peak = seq_array[(rmse_array>2.0) & (rmse_array<2.44)]
 
+    df = pd.read_csv('../dl_paper/SourceData_Figure_1.csv')
+    seqs_fig1 = df['Modified sequence'].str.replace('_','')
+    set_af = set(seq_array)
+    set_exp = set(seqs_fig1)
+    inters = set_af.intersection(set_exp)
+    df['Modified sequence'] = seqs_fig1
+    df.set_index('Modified sequence', inplace = True)
+    df_inters = df.loc[list(inters)]
+    
+    fig = plt.figure(figsize = (12,18))
+    ax=fig.add_subplot(111)
+    fig.set_size_inches((16, 8))
+    scatter = plt.scatter(df['m/z'], df['CCS'], c = df['Charge'], s = 0.01)
+    scatter2 = plt.scatter(df_inters['m/z'], df_inters['CCS'], c = 'green', s = 5)
+    plt.xlabel('m/z')
+    plt.ylabel(r'CCA ($A^2$)')
+    plt.title('Scatter plot: CCA vs m/z')
+    plt.legend(*scatter.legend_elements(), title = 'Charges')
 # %%
