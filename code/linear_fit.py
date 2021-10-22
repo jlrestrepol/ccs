795699431287cd9936215cf7dc6a963b0e9400f9
jlrestrepol <jlrestrepol@gmail.com>
2021-10-22 16:29:31 +0200

    Updates downloaded routin in linear_fit

--- code/linear_fit.py
+++ code/linear_fit.py
@@ -81,40 +81,52 @@ def predicted():
 
 #%%
 def downloaded():
-    data_folder = '../Data/AlphaFold/'
+    alpha_folder = '../Data/AlphaFold/'
+    combined_folder = '../Data/Combined/'
+    suffix = '-F1-model_v1.pdb'
     rmse_list = []
     seq_list = []
-    for root,dirs,files in os.walk(data_folder):
+    count_no_match = 0
+    i = 0
+    for root,dirs,file in os.walk(combined_folder):
+        df = pd.read_hdf(combined_folder+file)
+        df_filt = df[~df['Distance_matrix'].isna()]#Drop rows without distance matrix calculation
+        organism = file[:file.find('.')]#Get the organism from file name
+        pep_list = df_filt['Sequence'].values
 
-        if len(dirs) > 0:
-            continue
-        print(root)
-        for i, file in enumerate(files):
-            try: 
-                structure_alpha = Bio.PDB.PDBParser().get_structure("alpha",root+'/'+file)
-            except Exception:
-                pass
 
-            ppb=PPBuilder()
-            seq = str([pp.get_sequence() for pp in ppb.build_peptides(structure_alpha)][0])
-            seq_list.append(seq)
-            model_alpha = structure_alpha[0]
-            chain_a = model_alpha['A']
-            coords_one = np.fromiter( chain.from_iterable(res["CA"].coord for res in chain_a), dtype = 'f8', count = -1).reshape((-1, 3))
-            points = Points(coords_one)
-            line_fit = Line.best_fit(points)
-            residuals = np.fromiter((line_fit.distance_point(point) for point in points), dtype = 'f8', count = -1)
-            rmse = np.sqrt((residuals*residuals).sum()/coords_one.shape[0])
-            rmse_list.append(rmse)
-            if i%500 == 0:
-                print(f'{i} read out of {len(files)}')
+    for index, (label, prot_id) in enumerate(df_filt['Proteins'].items()):
+        #Get the data from the .pdb file
         
-    fig = plt.figure(figsize = (12,18))
-    ax=fig.add_subplot(111)
-    ax = sns.histplot(rmse_list)
-    ax.set_xlabel('RMSE')
-    ax.set_ylabel('Counts')
-    ax.set_title('Distribution of RMSE-PLDDT>70')
+        file_path = alpha_folder+prot_id+suffix
+        if not os.path.exists(file_path):#If protein name file is not present in Alpha Fold folder
+            count_no_match += 1
+            print(seq_list[index], prot_id+' not found, total not found: '+str(count_no_match), 'index: '+str(index))
+            continue
+
+        parser = Bio.PDB.PDBParser()# create parser
+        structure_alpha = parser.get_structure("alpha",alpha_folder+prot_id+suffix)#get structure
+        model_alpha = structure_alpha[0]#get model
+        chain_a = model_alpha['A']
+        ppb=PPBuilder()#polypeptide builder
+        seq_prot = str([pp.get_sequence() for pp in ppb.build_peptides(structure_alpha)][0])#get seq of protein
+        seq_pep = pep_list[index]#get seq of peptide
+        #Find position of peptide in protein
+        start_index = seq_prot.find(seq_pep)
+        final_index = start_index + len(seq_pep)
+        #Get 3D coords of residuals of the whole protein
+        coords_prot = np.fromiter( chain.from_iterable(res["CA"].coord for res in chain_a), dtype = 'f8', count = -1).reshape((-1, 3))
+        coords_one = coords_prot[start_index:final_index]#Extract coords of peptide                
+        #Perform the fit
+        points = Points(coords_one)
+        line_fit = Line.best_fit(points)
+        residuals = np.fromiter((line_fit.distance_point(point) for point in points), dtype = 'f8', count = -1)
+        rmse = np.sqrt((residuals*residuals).sum()/coords_one.shape[0])
+        rmse_list.append(rmse)
+        seq_list.append(seq_pep)
+        if i%500 == 0:
+            print(f'{i} read out of {len(df_filt.shape)} in organism {organism}')
+        i += 1
     
     return rmse_list, seq_list
 #%%
@@ -149,5 +161,14 @@ if __name__ == '__main__':
     plt.xlabel('m/z')
     plt.ylabel(r'CCA ($A^2$)')
     plt.title('Scatter plot: CCA vs m/z')
-    plt.legend(*scatter.legend_elements(), title = 'Charges')'''
+    plt.legend(*scatter.legend_elements(), title = 'Charges')
+    
+    fig = plt.figure(figsize = (12,18))
+    ax=fig.add_subplot(111)
+    ax = sns.histplot(rmse_list)
+    ax.set_xlabel('RMSE')
+    ax.set_ylabel('Counts')
+    ax.set_title('Distribution of RMSE-PLDDT>70')
+    
+    '''
 # %%
