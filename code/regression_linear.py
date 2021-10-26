f05ac66916f7fea2115edb25b9f583a86710f42f
Juan Desktop-CoxLab <jlrestrepol@gmail.com>
2021-10-26 15:27:15 +0200

    Adds plotting routine

--- code/regression_linear.py
+++ code/regression_linear.py
@@ -4,6 +4,7 @@ import pandas as pd
 import scipy.optimize
 import matplotlib.pyplot as plt
 import seaborn as sns
+import utils
 #%%
 class LinearRegression():
     def  __init__(self) -> None:
@@ -63,38 +64,50 @@ class LinearRegression():
             linear_ccs = df['linear_ccs']
             del df['linear_ccs']
             return linear_ccs
+    
+    def error_fit_plot(self, df):
+        if 'Charge' not in df:
+            raise Exception('df doesnt contain charge')
+        if 'm/z' not in df:
+            raise Exception('df doesnt contain m/z')
+        if self.f_2(0) is None:
+            raise Exception('f_2 is not trained')
+        if self.f_3(0) is None:
+            raise Exception('f_3 is not trained')
+        if self.f_4(0) is None:
+            raise Exception('f_4 is not trained')
+        
+        x_plot = np.linspace(250, 1700, 200)
+        fig = plt.gcf()
+        fig.set_size_inches((16, 8))
+        scatter = plt.scatter(df['m/z'], df['CCS'], c = df['Charge'], s = 0.01)
+        power2, = plt.plot(x_plot, lr.f_2(x_plot) , 'b--', label = 'fit charge 2')
+        power3, = plt.plot(x_plot, lr.f_3(x_plot) , 'b--', label = 'fit charge 3')
+        power4, = plt.plot(x_plot, lr.f_4(x_plot) , 'b--', label = 'fit charge 4')
+        plt.xlabel('m/z')
+        plt.ylabel(r'CCA ($A^2$)')
+        plt.title('Scatter plot: CCS vs m/z')
+        legend1 = plt.legend(*scatter.legend_elements(), title = 'Charges')
+        legend2 = plt.legend([power2, power3, power4], 
+        [f'{np.round(popt[0], 2)}*x + {np.round(popt[1], 2)}' for popt in [lr.args_f2, lr.args_f3, lr.args_f4]], loc = 4)
+        plt.gca().add_artist(legend1)
+
+        fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(18,6))
+        i = 0
+        for ax, df_it, f_i in zip(ax, [df[df['Charge']==2], df[df['Charge']==3], df[df['Charge']==4]],[lr.f_2, lr.f_3, lr.f_4]):
+            sns.histplot((df_it['CCS']-f_i(df_it['m/z']))/f_i(df_it['m/z'])*100, ax = ax)
+            ax.set_xlabel('Residual %')
+            ax.set_ylabel('Count')
+            ax.set_title(f'Charge {i+2}')
+            i += 1
 #%%
 if __name__ == "__main__":
     # Load data in
     df = pd.read_csv('../dl_paper/SourceData_Figure_1.csv')
+    df4 = pd.read_csv('../dl_paper/SourceData_Figure_4.csv')
+    df4['m/z'] = df4.apply(lambda x: utils.calculate_mass(x['Modified_sequence'], x['Charge']), axis = 1)
     lr = LinearRegression()
     lr.fit(df)
     lr.predict(df)  
-
-    #%%
-    # Plots
-    x_plot = np.linspace(250, 1700, 200)
-    fig = plt.gcf()
-    fig.set_size_inches((16, 8))
-    scatter = plt.scatter(df['m/z'], df['CCS'], c = df['Charge'], s = 0.01)
-    power2, = plt.plot(x_plot, lr.f_2(x_plot) , 'b--', label = 'fit charge 2')
-    power3, = plt.plot(x_plot, lr.f_3(x_plot) , 'b--', label = 'fit charge 3')
-    power4, = plt.plot(x_plot, lr.f_4(x_plot) , 'b--', label = 'fit charge 4')
-    plt.xlabel('m/z')
-    plt.ylabel(r'CCA ($A^2$)')
-    plt.title('Scatter plot: CCS vs m/z')
-    #plt.legend(*scatter.legend_elements(), title = 'Charges')
-    legend1 = plt.legend(*scatter.legend_elements(), title = 'Charges')
-    legend2 = plt.legend([power2, power3, power4], 
-    [f'{np.round(popt[0], 2)}*x + {np.round(popt[1], 2)}' for popt in [lr.args_f2, lr.args_f3, lr.args_f4]], loc = 4)
-    plt.gca().add_artist(legend1)
-
-    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(18,6))
-    i = 0
-    for ax, df_it, f_i in zip(ax, [df[df['Charge']==2], df[df['Charge']==3], df[df['Charge']==4]],[lr.f_2, lr.f_3, lr.f_4]):
-        sns.distplot((df_it['CCS']-f_i(df_it['m/z']))/f_i(df_it['m/z'])*100, ax = ax)
-        ax.set_xlabel('Residual %')
-        ax.set_ylabel('Count')
-        ax.set_title(f'Charge {i+2}')
-        i += 1
+    lr.error_fit_plot(df)
 # %%
--- code/regression_power.py
+++ code/regression_power.py
@@ -4,6 +4,7 @@ import pandas as pd
 import scipy.optimize
 import matplotlib.pyplot as plt
 import seaborn as sns
+import utils
 #%%
 class PowerLawRegression():
     def  __init__(self) -> None:
@@ -63,40 +64,52 @@ class PowerLawRegression():
             power_ccs = df['power_ccs']
             del df['power_ccs']
             return power_ccs
+    
+    def error_fit_plot(self, df):
+        if 'Charge' not in df:
+            raise Exception('df doesnt contain charge')
+        if 'm/z' not in df:
+            raise Exception('df doesnt contain m/z')
+        if self.f_2(0) is None:
+            raise Exception('f_2 is not trained')
+        if self.f_3(0) is None:
+            raise Exception('f_3 is not trained')
+        if self.f_4(0) is None:
+            raise Exception('f_4 is not trained')
+
+        x_plot = np.linspace(250, 1700, 200)
+        fig = plt.gcf()
+        fig.set_size_inches((16, 8))
+        scatter = plt.scatter(df['m/z'], df['CCS'], c = df['Charge'], s = 0.01)
+        power2, = plt.plot(x_plot, pl.f_2(x_plot) , 'b--', label = 'fit charge 2')
+        power3, = plt.plot(x_plot, pl.f_3(x_plot) , 'b--', label = 'fit charge 3')
+        power4, = plt.plot(x_plot, pl.f_4(x_plot) , 'b--', label = 'fit charge 4')
+        plt.xlabel('m/z')
+        plt.ylabel(r'CCA ($A^2$)')
+        plt.title('Scatter plot: CCS vs m/z')
+        #plt.legend(*scatter.legend_elements(), title = 'Charges')
+        legend1 = plt.legend(*scatter.legend_elements(), title = 'Charges')
+        legend2 = plt.legend([power2, power3, power4], 
+        [f'{np.round(popt[0], 2)}*x + {np.round(popt[1], 2)}' for popt in [pl.args_f2, pl.args_f3, pl.args_f4]], loc = 4)
+        plt.gca().add_artist(legend1)
+
+        fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(18,6))
+        i = 0
+        for ax, df_it, f_i in zip(ax, [df[df['Charge']==2], df[df['Charge']==3], df[df['Charge']==4]],[pl.f_2, pl.f_3, pl.f_4]):
+            sns.histplot((df_it['CCS']-f_i(df_it['m/z']))/f_i(df_it['m/z'])*100, ax = ax)
+            ax.set_xlabel('Residual %')
+            ax.set_ylabel('Count')
+            ax.set_title(f'Charge {i+2}')
+            i += 1
+
 #%%
 if __name__ == "__main__":
     # Load data in
     df = pd.read_csv('../dl_paper/SourceData_Figure_1.csv')
+    df4 = pd.read_csv('../dl_paper/SourceData_Figure_4.csv')
+    df4['m/z'] = df4.apply(lambda x: utils.calculate_mass(x['Modified_sequence'], x['Charge']), axis = 1)
     pl = PowerLawRegression()
     pl.fit(df)
-    pl.predict(df)  
-
-    #%%
-    # Plots
-    x_plot = np.linspace(250, 1700, 200)
-    fig = plt.gcf()
-    fig.set_size_inches((16, 8))
-    scatter = plt.scatter(df['m/z'], df['CCS'], c = df['Charge'], s = 0.01)
-    power2, = plt.plot(x_plot, pl.f_2(x_plot) , 'b--', label = 'fit charge 2')
-    power3, = plt.plot(x_plot, pl.f_3(x_plot) , 'b--', label = 'fit charge 3')
-    power4, = plt.plot(x_plot, pl.f_4(x_plot) , 'b--', label = 'fit charge 4')
-    plt.xlabel('m/z')
-    plt.ylabel(r'CCA ($A^2$)')
-    plt.title('Scatter plot: CCS vs m/z')
-    #plt.legend(*scatter.legend_elements(), title = 'Charges')
-    legend1 = plt.legend(*scatter.legend_elements(), title = 'Charges')
-    legend2 = plt.legend([power2, power3, power4], 
-    [f'{np.round(popt[0], 2)}*x + {np.round(popt[1], 2)}' for popt in [pl.args_f2, pl.args_f3, pl.args_f4]], loc = 4)
-    plt.gca().add_artist(legend1)
-
-    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(18,6))
-    i = 0
-    for ax, df_it, f_i in zip(ax, [df[df['Charge']==2], df[df['Charge']==3], df[df['Charge']==4]],[pl.f_2, pl.f_3, pl.f_4]):
-        sns.distplot((df_it['CCS']-f_i(df_it['m/z']))/f_i(df_it['m/z'])*100, ax = ax)
-        ax.set_xlabel('Residual %')
-        ax.set_ylabel('Count')
-        ax.set_title(f'Charge {i+2}')
-        i += 1
-# %%
-
+    pl.predict(df4)  
+    pl.error_fit_plot(df4)
 # %%
